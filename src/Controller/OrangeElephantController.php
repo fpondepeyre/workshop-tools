7e9fbd27dc796f01fcf6753617cac65458116c6e
Florian Pondepeyre <fpondepeyre@lefigaro.fr>
2018-12-14 17:03:29 +0100

    fix: fix grumphp error

--- src/Controller/OrangeElephantController.php
+++ src/Controller/OrangeElephantController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrangeElephantController extends AbstractController
@@ -10,20 +11,10 @@ class OrangeElephantController extends AbstractController
     /**
      * @Route("/orange/elephant", name="orange_elephant")
      */
-    public function index()
+    public function index(): Response
     {
         return $this->render('gentle_elephant/index.html.twig', [
             'controller_name' => 'GentleElephantController',
         ]);
-    }
-
-    public function sluggify(string $string, string $separator = '-', int $maxLength = 96): string
-    {
-        $title = iconv('UTF-8', 'ASCII//TRANSLIT', $string);
-        $title = preg_replace("%[^-/+|\w ]%", '', $title);
-        $title = strtolower(trim(substr($title, 0, $maxLength), '-'));
-        $title = preg_replace("/[\/_|+ -]+/", $separator, $title);
-
-        return $title;
     }
 }
